1f4f61cc4273dbbd78e393d854fa4673eb792c86
Bertys <bertysitu@gmail.com>
2023-07-19 22:58:01 +0200

    Update nftlasty.sol
    Update despres de molt temps, nft factory per mitja duna URI

--- nftlasty.sol
+++ nftlasty.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract MyNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
